7dc25d6ebc78ffdf6c35fc7e2c9c9f385b1e06ef
Jorg Thuijls <jorg.thuijls@holcim.com>
2015-08-26 17:32:51 +1000

    minor fixes

--- src/controller/App.controller.js
+++ src/controller/App.controller.js
@@ -4,12 +4,20 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("holcim.swipedemo.controller.App", {
+		/**
+		 * On Initialization of the controller, this is the method that'll get executed
+		 * @public
+		 */
 		onInit: function() {
 			var model = sap.ui.getCore().getModel('i18n');
 			console.log(model.getProperty('master.title'));
 			this._setupTransitions();
 		},
 
+		/**
+		 * Sets up the swipe event and attached a handler
+		 * @private
+		 */
 		_setupTransitions: function() {
 			$('body').on('swipeleft', '.swipe-page', function(e) {
 				this._navigate(e, 'left');
@@ -20,11 +28,18 @@ sap.ui.define([
 			}.bind(this));
 		},
 
+		/**
+		 * Navigates to a specific page in the application based on the
+		 * current page and the direction of swiping
+		 * @param  {Object} e         the swype event
+		 * @param  {string} direction the direction, either 'left' or 'right'
+		 * @private
+		 */
 		_navigate: function(e, direction) {
 			var id = e.currentTarget.id;
 			var match = id.match(/(.*swipe-page)([0-9]{1,})/);
 			var add = (direction === 'left') ? 1 : -1;
-			if (match.length > 2) {
+			if (match && match.length > 2) {
 				this.byId('swipedemo-app').to(match[1] + (Number(match[2]) + add), 'slide-' + direction);
 			}
 		}
